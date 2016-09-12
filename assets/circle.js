e216ebeb91a851f6196ce50aa37e4a634338d59c
adi <adityaa803@gmail.com>
2016-09-12 20:26:44 +0530

    'integrated_skins_adding_feature'

--- assets/circle.js
+++ assets/circle.js
@@ -1,14 +1,13 @@
 $(document).ready(function(){
     var prefs = {
-        element : ".correctTimer"
+        element : ".circlebar"
     };
-    $('.correctTimer').each(function () {
-        new timer(prefs);
+    $('.circlebar').each(function () {
+        new circlebar(prefs);
     });
 });
-function timer(prefs){
+function circlebar(prefs){
     this.element = $(prefs.element);
-    console.log(this.element);
     this.element.append('<div class="spinner-holder-one animate-0-25-a"><div class="spinner-holder-two animate-0-25-b"><div class="loader-spinner" style=""></div></div></div><div class="spinner-holder-one animate-25-50-a"><div class="spinner-holder-two animate-25-50-b"><div class="loader-spinner"></div></div></div><div class="spinner-holder-one animate-50-75-a"><div class="spinner-holder-two animate-50-75-b"><div class="loader-spinner"></div></div></div><div class="spinner-holder-one animate-75-100-a"><div class="spinner-holder-two animate-75-100-b"><div class="loader-spinner"></div></div></div>');
     this.time = 0;
     this.maxTime = 60;
@@ -17,21 +16,26 @@ function timer(prefs){
     this.size = 150;
     this.fontSize = 20;
     this.fontColor = "rgb(135, 206, 235)";
+    this.skin = ""
     var percentage = 0, that = this, date = 0;
-    var attribs = this.element.find("div")[0].parentNode.dataset;
+    // var attribs = this.element.find("div")[0].parentNode.dataset;
+    var attribs = this.element[0].dataset;
     this.initialise = function(){
-        that.time = parseInt(attribs.timerStarttime) || parseInt(prefs.startTime) || 0;
-        that.maxTime = parseInt(attribs.timerMaxtime) || parseInt(prefs.maxTime) || 60;
-        that.counter = parseInt(attribs.timerCounter) || parseInt(prefs.counter) || 1000;
-        that.dialWidth = parseInt(attribs.timerDialwidth) || parseInt(prefs.dialWidth) || 5;
-        that.size = attribs.timerSize || prefs.size || "150px";
-        that.fontSize = attribs.timerFontsize || prefs.fontSize || "20px";
-        that.fontColor = attribs.timerFontcolor || prefs.fontColor || "rgb(135, 206, 235)";
+        that.time = parseInt(attribs.circleStarttime) || parseInt(prefs.startTime) || 0;
+        that.maxTime = parseInt(attribs.circleMaxtime) || parseInt(prefs.maxTime) || 60;
+        that.counter = parseInt(attribs.circleCounter) || parseInt(prefs.counter) || 1000;
+        that.dialWidth = parseInt(attribs.circleDialwidth) || parseInt(prefs.dialWidth) || 5;
+        that.size = attribs.circleSize || prefs.size || "150px";
+        that.fontSize = attribs.circleFontsize || prefs.fontSize || "20px";
+        that.fontColor = attribs.circleFontcolor || prefs.fontColor || "rgb(135, 206, 235)";
+        that.skin = attribs.circleSkin || prefs.skin || " ";
 
         that.element.find(".loader-bg").css("border-width",that.dialWidth+"px");
         that.element.find(".loader-spinner").css("border-width",that.dialWidth+"px");
         that.element.css({"width":that.size,"height":that.size});
-        that.element.find(".loader-bg .text").css({"font-size":that.fontSize,"color":that.fontColor});
+        that.element.find(".loader-bg .text")
+            .css({"font-size":that.fontSize,"color":that.fontColor});
+        that.element.addClass(that.skin);
     };
     this.initialise();
     this.timer = setInterval(function(){
